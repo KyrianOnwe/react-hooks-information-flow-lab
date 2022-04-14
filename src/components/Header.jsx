ed377ad1c62d21e48c45bdcd829c1e68ba903719
Kyrian <dr.kyrian.onwe@gmail.com>
2022-04-14 18:44:54 -0500

    commit tree a3ac50d693bdc56287f4ed0e4f4c1d11c1e80e31

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header(props){
+    console.log(props)
+    return (
+        <header>
+         <h2>Shopster</h2>
+         <button onClick={ props.onDarkModeClick }>
+         {props.isDarkMode ? "Dark" : "Light"} Mode
+         </button>
+      </header>
+    )
+
+}
+
+export default Header
